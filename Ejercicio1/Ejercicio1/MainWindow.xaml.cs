8a5180ca29e8850d1ebaba64ac919b860c82f141
Ismael Bueno <IsmaelBMolina@gmail.com>
2018-03-28 19:04:49 +0200

    Añadido el ejercicio 1,2 y 3

---
+++ Ejercicio1/Ejercicio1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Ejercicio1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbx_numero.Focus();
+        }
+
+        private void btn_calcular_Click(object sender, RoutedEventArgs e)
+        {
+            double resultado = 0;
+            int limite;
+
+            try
+            {
+                limite = int.Parse(tbx_numero.Text);
+                for (int i = 0; i <= limite; i++)
+                {
+                    resultado += i;
+                }
+
+                lbl_resultado.Content = resultado.ToString();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Valor incorrecto");
+                tbx_numero.Focus();
+                tbx_numero.Clear();
+            }
+        }
+
+        private void btn_cancelar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void tbx_numero_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Ejercicio3/Ejercicio3/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int numero;
+        static int intentos = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_numero_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            numero = rnd.Next(1, 101);
+            btn_probar.IsEnabled = true;
+            chx.IsEnabled = true;
+            btn_numero.IsEnabled = false;
+        }
+
+        private void chx_Checked(object sender, RoutedEventArgs e)
+        {
+            lbl_vernumero.Content = numero;
+        }
+
+        private void chx_Unchecked(object sender, RoutedEventArgs e)
+        {
+            lbl_vernumero.Content = string.Empty;
+        }
+
+        private void btn_probar_Click(object sender, RoutedEventArgs e)
+        {
+            int posible_resultado;
+
+            try
+            {
+                posible_resultado = int.Parse(txb_resultado.Text);
+
+                if (posible_resultado<1 || posible_resultado > 100) //valor mínimo y máximo para adivinar
+                {
+                    lbl_resultado.Content = "El valor introducido no es correcto";
+                }
+                else if (posible_resultado > numero)
+                {
+                    intentos++;
+                    lbl_resultado.Content = "El número buscado es menor";
+                } else if(posible_resultado < numero)
+                {
+                    intentos++;
+                    lbl_resultado.Content = "El número buscado es mayor";
+                }
+                else if (posible_resultado == numero)
+                {
+                    intentos++;
+                    MessageBox.Show("Acertaste en " + intentos + " intentos");
+
+                    //Al acertar se restablecen los valores por defecto
+                    intentos = 0;
+                    btn_numero.IsEnabled = true;
+                    btn_probar.IsEnabled = false;
+                    chx.IsEnabled = false;
+                    chx.IsChecked = false;
+                    lbl_vernumero.Content = string.Empty;
+                    lbl_resultado.Content = string.Empty;                  
+                }
+                txb_resultado.Focus();
+
+            }
+            catch (Exception)
+            {
+                lbl_resultado.Content = "El valor introducido no es correcto";
+            }
+        }
+    }
+}
