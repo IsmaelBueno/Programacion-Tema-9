4e8db91559626abab10c3c4cac302b58e1f9a07f
Ismael Bueno <IsmaelBMolina@gmail.com>
2018-05-08 21:54:45 +0200

    Añadido el ejercicio 14

---
+++ Ejercicio14/Ejercicio14/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//---------------------------------
+using System.IO;
+using Microsoft.Win32;
+
+namespace Ejercicio14
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string ruta = @"C:\";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void evento_btn_selecionarFichero(object sender, RoutedEventArgs e)
+        {
+            //Selección del archivo
+            OpenFileDialog vtn_abrirArchivo = new OpenFileDialog();
+
+            vtn_abrirArchivo.Filter = "Textos|*.txt";
+            vtn_abrirArchivo.InitialDirectory = ruta;
+
+            bool? seleccion = vtn_abrirArchivo.ShowDialog();
+
+            //Extraer información del archivo
+            char[] separadorLineas = { '\n' };
+            char[] separadorPalabras = { ' ', '_', '\t', '\n' };
+            string texto = string.Empty;
+            FileInfo informacionFichero;
+
+            if (seleccion == true)
+            {
+                ruta = vtn_abrirArchivo.FileName;
+
+                using(StreamReader lector = new StreamReader(ruta))
+                {
+                    texto = lector.ReadToEnd();
+                }
+
+                string[] lineas = texto.Split(separadorLineas, StringSplitOptions.RemoveEmptyEntries);
+                string[] palabras = texto.Split(separadorPalabras, StringSplitOptions.RemoveEmptyEntries);
+                informacionFichero = new FileInfo(ruta);
+
+                lbl_numeroLineas.Content = lineas.Length;
+                lbl_numeroPalabras.Content = palabras.Length;
+                lbl_ruta.Content = informacionFichero.Directory;
+                lbl_tamanio.Content = informacionFichero.Length;
+                lbl_nombreArchivo.Content = informacionFichero.Name;
+                lbl_FechaCreacion.Content = informacionFichero.CreationTime;
+            }
+
+
+        }
+    }
+}
